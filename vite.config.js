eff8be5b9c1328974faf207d8a488f75b0c357fe
SpookDaKiid2 <William.Mattox@uga.edu>
2025-07-17 14:13:52 +0000

    Fix Vite config for Netlify deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      // Tell Vite not to try bundling this module (fixes Netlify build errors)
+      external: ['uuid'],
+    },
+  },
 });
+
 
